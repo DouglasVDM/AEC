d4a5f54b8a54fdb0f86cd27ea5823920480085eb
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-10-15 21:53:54 +0200

    Update ShowProposalInfo.js

--- client/src/pages/dashboards/dashComponents/ShowProposalInfo.js
+++ client/src/pages/dashboards/dashComponents/ShowProposalInfo.js
@@ -78,11 +78,11 @@ const ShowProposalInfo = ({ setPage, proposal }) => {
 							</div>
 							<h5>Start Up Cost</h5>
 							<div className="proposal-info">
-								<p>{info.startup_cost}</p>
+								<p>{info.startup_costs}</p>
 							</div>
 							<h5>Operational Cost</h5>
 							<div className="proposal-info">
-								<p>{info.operational_cost}</p>
+								<p>{info.operational_costs}</p>
 							</div>
 							<h5>Finance Plan</h5>
 							<div className="proposal-info">
